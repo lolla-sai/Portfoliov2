36f5ebfa806366e50212e4964bd2ec66f5d4fe76
lolla-sai <saisameer.lolla@gmail.com>
2022-08-14 00:16:06 +0530

    corrected font rendering error

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
     return (
         <Html>
-            <Head />
+            <Head>
+                <link
+                    href="assets/fonts/font_declaration.css"
+                    rel="stylesheet"
+                />
+            </Head>
             <body className="bg-[#0a192f] font-custom-sans scroll-smooth !leading-[1.3] text-lg md:text-xl">
                 <Main />
                 <NextScript />
